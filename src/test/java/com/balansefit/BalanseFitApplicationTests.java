1227bdc9b98ac81955be01b803400cd24f81a4b7
Seok-Kyungmin <sukm38621@gmail.com>
2022-04-04 09:34:52 +0900

    Initial commit

---
+++ src/test/java/com/balansefit/BalanseFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balansefit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalanseFitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
